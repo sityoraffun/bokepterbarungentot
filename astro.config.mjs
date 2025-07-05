8562e7827a0d48e011c59a4b1600522950de02c0
sityoraffun <sityoraffun@gmail.com>
2025-07-05 11:10:21 +0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
-  site: 'https://cewekdigangbang.pages.dev',
+  site: 'https://bokepterbarungentot.pages.dev',
   output: 'server',
   adapter: cloudflare(),
 });
